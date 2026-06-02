358c14686faee04684ec5826fde006879b0bf122
dbstylesnet <db@dbstyles.net>
2026-06-02 20:54:54 +0200

    Added nativewind

--- app/_layout.js
+++ app/_layout.js
@@ -17,7 +17,7 @@ const Layout = () => {
             />
           </View>
         </SafeAreaView>
-        <View className="min-h-0 flex-1">
+        <View className="h-[80vh] min-h-0">
           <Slot />
         </View>
       </View>
